19f260015e204626a303cb6011a3e43b447bc4ce
SelinaLingY <sling@stu.naperville203.org>
2023-01-20 11:23:46 -0600

    changed tester into objects

--- geneticRep.js
+++ geneticRep.js
@@ -5,57 +5,28 @@
 
 let teacherTester = 'Sissel';
 let repDataTester = [
-    [1, 'Programming 1', '123', null],
-    [2, 'Programming 1', '123', null],
-    [4, 'Programming 2', '123', null],
-    [7, 'AP Com Sci', '121', null]
-    [8, 'Consumer Ec', '128', 'Lynne']
+    {period: 1, class: 'Programming 1', room: '123', coTeacher: null},
+    {period: 2, class: 'Programming 1', room: '123', coTeacher: null},
+    {period: 4, class: 'Programming 2', room: '123', coTeacher: null},
+    {period: 7, class: 'AP Com Sci', room: '121', coTeacher: null},
+    {period: 8, class: 'Consumer Ec', room: '128', coTeacher: 'Lynne'}
 ];
 // FIXME: update with the created classes
 // Currently [Period, Class, Room, Co-Teacher]
 
-<div class="container my-3">
-    <table class="table table-borderless">
-        <thead>
-            <tr>
-                <th>Teacher</th>
-                <th>Period 1</th>
-                <th>Period 2</th>
-                <th>Homeroom</th>
-                <th>WIN</th>
-                <th>Period 3</th>
-                <th>Period 4</th>
-                <th>Period 5</th>
-                <th>Period 6</th>
-                <th>Period 7</th>
-                <th>Period 8</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td id="teacherName"></td>
-                <td id="p1"></td>
-                <td id="p2"></td>
-                <td id="Homeroom"></td>
-                <td id="WIN"></td>
-                <td id="p3"></td>
-                <td id="p4"></td>
-                <td id="p5"></td>
-                <td id="p6"></td>
-                <td id="p7"></td>
-                <td id="p8"></td>
-            </tr>
-        </tbody>
-    </table>
-</div>
 
 document.getElementById("teacherName").innerHTML = teacherTester;
 
 for(var i = 0 ; i < repDataTester.length ; i++){
     var repDataRows = repDataTester[i];
     
-    System.out.println(repDataRows[0]);
+    //Java format, use   document.getElementById(id).innerHTML = ____;
+    //System.out.println(repDataRows.period);
+    //System.out.println(repDataRows.class);
+    //System.out.println(repDataRows.room);
+    //System.out.println(repDataRows.coTeacher);
 //https://stackoverflow.com/questions/6458128/accessing-javascript-variable-stored-in-an-array
+//https://stackoverflow.com/questions/35995273/how-to-run-html-file-using-node-js 
 //might help
 }
 /*
