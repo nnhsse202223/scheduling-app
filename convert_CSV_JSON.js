2dc6fff9155c9ce919d7440d706848a70456c75e
MaxNguyen5040 <maxnguyen3012@gmail.com>
2023-01-25 12:03:39 -0600

    worked on

---
+++ convert_CSV_JSON.js
@@ -0,0 +1,18 @@
+let csvToJson = require('convert-csv-to-json');
+let fs = require('fs');
+
+
+// let json = csvToJson.parseSubArray(',',',').getJsonFromCsv('CustomClassData.csv');
+let fileOutputName = 'data.json';
+let fileInputName = 'classDataBackup.csv'
+
+let json = csvToJson.getJsonFromCsv(fileInputName);
+for(let i=0; i<json.length;i++){
+   console.log(json[i]);
+}
+
+
+//console.log(json.toString);
+//json = JSON.stringify(json);
+//fs.writefile("data.json",json);
+
