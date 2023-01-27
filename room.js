0d840896b9cca569a65f3fcf7ff100e6e753d234
Aarav-Mistry <aarav.b.mistry@gmail.com>
2023-01-27 11:02:41 -0600

    quick commits

--- room.js
+++ room.js
@@ -5,9 +5,10 @@ class Room
     {
         this.room_no = room_no;
         this.type = type;
+        this.teacher;
+        this.classer;
         //JSON.stringify(this.type)
     }
-
 
     //get functions
     get room_type()
@@ -18,6 +19,26 @@ class Room
     get room_number()
     {
         return this.room_no;
+    }
+
+    get room_teacher()
+    {
+        return this.teacher;
+    }
+
+    get room_class()
+    {
+        return this.classer;
+    }
+
+    set room_teach(teach)
+    {
+        this.teacher = teach;
+    }
+
+    set room_classy(clash)
+    {
+        this.classer = clash;
     }
 
 }
