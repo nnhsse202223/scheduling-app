4d1f5a92cd90e7b4ee0c6aa157b6d651a2cda1b5
MaxNguyen5040 <maxnguyen3012@gmail.com>
2023-02-06 11:27:06 -0600

    Seperated some json data into class,room

--- connections_access.js
+++ connections_access.js
@@ -1,13 +1,13 @@
 const { MongoClient } = require("mongodb");
 require('dotenv').config();
-let fs = require('fs');
-
-
 
 // Replace the uri string with your MongoDB deployment's connection string.
 const uri = "mongodb+srv://"+process.env.ADMIN_USERNAME+":"+process.env.ADMIN_PASSWORD+"@cluster0.gnworbx.mongodb.net/?retryWrites=true&w=majority";
 console.log(uri);
 let json_data = []
+let teacher_array = [];
+let room_array = [];
+let class_array = [];
 
 
 
@@ -19,12 +19,59 @@ async function run() {
     const database = client.db("NNHS_DATA");
     const coll = database.collection("DATA");
 
+    //Accessing all data in MongoDB collection
     const cursor = coll.find();
-    // iterate code goes here
-    await cursor.forEach( function(myDoc) { json_data.push(myDoc); } );
-    console.log(json_data);
 
+    //Saving data to local variable
+    await cursor.forEach( function(myDoc) { json_data.push(myDoc); } );
+    //console.log(json_data);
     
+    //Separating data into just classes
+    for (const element of json_data) {
+      class_array.push(element["Class"]);
+    }
+
+    //Separating data into just teachers
+    for (const element of json_data) {
+      if(Array.isArray(element["Teachers"])){
+        for(const subArrayElement of element["Teachers"]) {
+          if(!(teacher_array.includes(subArrayElement))){
+            teacher_array.push(subArrayElement);
+          }
+        }
+      }
+
+      else{
+        if(!(teacher_array.includes(element["Teachers"]))){
+          teacher_array.push(element["Teachers"]);
+        }
+      }
+
+    }
+
+    //Separating data into just rooms
+    for (const element of json_data) {
+      if(Array.isArray(element["Room"])){
+        for(const subArrayElement of element["Room"]) {
+          if(!(room_array.includes(subArrayElement))){
+            room_array.push(subArrayElement);
+          }
+        }
+      }
+
+      else{
+        if(!(room_array.includes(element["Room"]))){
+          room_array.push(element["Room"]);
+        }
+      }
+
+    }
+
+    //Printing out result arrays
+    console.log(teacher_array);
+    console.log(room_array);
+    console.log(class_array);
+ 
 
   } finally {
     // Ensures that the client will close when you finish/error
