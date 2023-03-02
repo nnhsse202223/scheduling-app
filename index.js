ba9aa6bf2c2745dfd2a186ef9f516ebd33fb7ef6
Joonoothoon <jaqunell@stu.naperville203.org>
2023-03-02 11:48:12 -0600

    Algorithm now logs schedules

--- index.js
+++ index.js
@@ -11,13 +11,56 @@ app.get('/',(req,res)=>{
 app.get('/database', async (req,res)=>{
     let csvString = await script();
     fileNames = fs.readdirSync(__dirname + "/Logs");
-    console.log(fileNames);
-    let n = 0;
-    fileNames.forEach(name => {
-        name = name.subString(11);
-        name
-    })
-    fs.writeFileSync("Logs/ScheduleLog1.csv",csvString,(err) => err && console.error(err));
+    if(fileNames.length >= 10)
+    {
+        console.log('what');
+        var fileToDelete = fileNames[0];
+        fileNames.forEach(name => {
+            var check = +name.substring(11, name.indexOf('_')) < +fileToDelete.substring(11, fileToDelete.indexOf('_'));
+            var check2 = +name.substring(11, name.indexOf('_')) == +fileToDelete.substring(11, fileToDelete.indexOf('_'));
+            if(check)
+            {
+                fileToDelete = name;
+            }
+            else if(check2)
+            {
+                var check3 = +name.substring(name.indexOf('_') + 1, name.indexOf('.')) < +fileToDelete.substring(fileToDelete.indexOf('_') + 1, fileToDelete.indexOf('.'));
+                if(check3)
+                {
+                    fileToDelete = name;
+                }
+            }
+        })
+        fs.unlinkSync("Logs/" + fileToDelete, (err) => err && console.error(err));
+    }
+    var today = new Date();
+    var dateTime = "ScheduleLog" + today.getFullYear();
+    if(today.getMonth() + 1 < 10)
+    {
+        dateTime += 0;
+    }
+    dateTime += today.getMonth()+1;
+    if(today.getDate() < 10)
+    {
+        dateTime += 0;
+    }
+    dateTime += today.getDate() + "_";
+    if(today.getHours() < 10)
+    {
+        dateTime += 0;
+    }
+    dateTime += today.getHours();
+    if(today.getMinutes() < 10)
+    {
+        dateTime += 0;
+    }
+    dateTime += today.getMinutes();
+    if(today.getSeconds() < 10)
+    {
+        dateTime += 0;
+    }
+    dateTime += today.getSeconds();
+    fs.writeFileSync("Logs/" + dateTime + ".csv",csvString,(err) => err && console.error(err));
     res.send(csvString);
 })
 
