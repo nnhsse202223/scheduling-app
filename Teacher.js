d9c19898b9042f59ed37b4bd15e807faf1731d82
Aarav-Mistry <aarav.b.mistry@gmail.com>
2023-04-21 10:45:27 -0500

    someone coded something, idk
    Co-authored-by: MaxNguyen5040 <MaxNguyen5040@users.noreply.github.com>

--- Teacher.js
+++ Teacher.js
@@ -5,7 +5,7 @@ class Teacher
     {
         this.name = name;
         this.classPeriodList = [1,2,3,4,5,6,7,8];
-        this.otherROOOMList = [];
+        this.otherROOOMList = [0,0,0,0,0,0,0,0];
     }
 
     //NOTE: ADD 2 seperate arrays, one that contains room numbers with period as the index+1, and one with classes. 
@@ -23,9 +23,14 @@ class Teacher
         return this.classPeriodList;
     }
 
-    fill_class(index_of_class_to_fill) // adds a class to a teachers' schedule: basically if they have a number that period is free, if they have a 0 in that slot they have a class. 
+    fill_class(index_of_class_to_fill,class_name) // adds a class to a teachers' schedule: basically if they have a number that period is free, if they have a 0 in that slot they have a class. 
     {
-        this.classPeriodList[index_of_class_to_fill] = 0;    
+        this.classPeriodList[index_of_class_to_fill] = class_name;    
+    }
+
+    addLunch()
+    {
+        this.classPeriodList[Math.trunc(Math.random() * (6 - 4) + 4)]  = "Lunch";
     }
 
     reset()
